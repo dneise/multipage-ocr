e5c5cf433a0f53c1179d8b9c3e475a66bad1abe5
Dominik Neise <neised@phys.ethz.ch>
2017-08-16 10:05:36 +0200

    make lines short enough

--- multipage-ocr.py
+++ multipage-ocr.py
@@ -5,8 +5,8 @@ Usage:
   multipage-ocr.py <input_path> <output_path> [options]
 
 Options:
-  --density N           dpi density to supply to ImageMagick convert [default: 300]
-  --depth N             bit depth [default: 8]
+  --density N           dpi density for ImageMagick convert [default: 300]
+  --depth N             bit depth for ImageMagick convert [default: 8]
   --imageformat FORMAT  image format (e.g., jpg, png, tif) [default: jpg]
   --psm N               tesseract layout analysis mode [default: 3]
 """
@@ -20,24 +20,29 @@ from glob import glob
 from io import StringIO
 
 
-def main(arguments):
-    arguments['<input_path>'] = os.path.realpath(arguments['<input_path>'])
-    arguments['<output_path>'] = os.path.realpath(arguments['<output_path>'])
-    arguments['--density'] = int(arguments['--density'])
-    arguments['--depth'] = int(arguments['--depth'])
-    arguments['--psm'] = int(arguments['--psm'])
-    assert os.path.exists(arguments['<input_path>'])
-    assert arguments['<input_path>'].endswith(".pdf")
+def main(args):
+    args['<input_path>'] = os.path.realpath(args['<input_path>'])
+    args['<output_path>'] = os.path.realpath(args['<output_path>'])
+    args['--density'] = int(args['--density'])
+    args['--depth'] = int(args['--depth'])
+    args['--psm'] = int(args['--psm'])
+    assert os.path.exists(args['<input_path>'])
+    assert args['<input_path>'].endswith(".pdf")
 
-    num_pages = PdfFileReader(open(arguments['<input_path>'], 'rb')).getNumPages()
-
-    #  with tempfile.TemporaryDirectory(suffix='', prefix='tmp', dir='.') as tmp_dir:
-    tmp_dir = tempfile.mkdtemp(suffix='', prefix='multipage_ocr_temp_', dir='.')
+    num_pages = PdfFileReader(open(args['<input_path>'], 'rb')).getNumPages()
+    tmp_dir = tempfile.mkdtemp(
+        prefix='multipage_ocr_temp_',
+        dir='.'
+    )
 
     for i in trange(num_pages):
-        tmp_file_path = os.path.join(tmp_dir, '{page:010d}.{imageformat}'.format(
-            page=i,
-            imageformat=arguments['--imageformat']))
+        tmp_file_path = os.path.join(
+            tmp_dir,
+            '{page:010d}.{imageformat}'.format(
+                page=i,
+                imageformat=args['--imageformat']
+            )
+        )
 
         cmd = '''convert \
         -density {density}\
@@ -45,9 +50,9 @@ def main(arguments):
         {input_path}[{page}]\
         -background white\
         {tmp_file_path}'''.format(
-            density=arguments['--density'],
-            depth=arguments['--depth'],
-            input_path=arguments['<input_path>'],
+            density=args['--density'],
+            depth=args['--depth'],
+            input_path=args['<input_path>'],
             page=i,
             tmp_file_path=tmp_file_path
             )
@@ -61,16 +66,16 @@ def main(arguments):
         {text_file_path} \
         > /dev/null 2>&1
         '''.format(
-            psm=arguments['--psm'],
+            psm=args['--psm'],
             tmp_file_path=tmp_file_path,
             text_file_path=text_file_path)
         os.system(cmd)
 
-    with open(arguments['<output_path>'], 'wt') as outfile:
+    with open(args['<output_path>'], 'wt') as outfile:
         for path in sorted(glob(os.path.join(tmp_dir, '*.text'))):
             outfile.write(open(path).read())
 
 if __name__ == '__main__':
-    arguments = docopt(__doc__, version='Naval Fate 2.0')
-    print(arguments)
-    main(arguments)
+    args = docopt(__doc__, version='Naval Fate 2.0')
+    print(args)
+    main(args)
