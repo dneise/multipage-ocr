4e2f4ca51667aeeef1c3597063c68ee99c42dc05
Dominik Neise <neised@phys.ethz.ch>
2017-08-16 09:29:58 +0200

    remove rest of getopt remove optional output file, now needed

--- multipage-ocr.py
+++ multipage-ocr.py
@@ -2,7 +2,7 @@
 """Naval Fate.
 
 Usage:
-  multipage-ocr.py <input_file> <output_file> [options]
+  multipage-ocr.py <input_path> <output_path> [options]
 
 Options:
   --density N           dpi density to supply to ImageMagick convert [default: 300]
@@ -15,6 +15,7 @@ Options:
 from docopt import docopt
 import sys
 import os
+import os.path
 import string
 import random
 from PyPDF2 import PdfFileReader
@@ -30,64 +31,28 @@ def id_generator(size=10, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for x in range(size))
 
 
-def main(argv):
+def main(arguments):
+    '''arguments example:
+    {'--clean': False,
+     '--density': '300',
+     '--depth': '8',
+     '--imageformat': 'jpg',
+     '--psm': '3',
+     '--quiet': False,
+     '<input_path>': 'in',
+     '<output_path>': 'out'}
 
-    input_file = None
-    output_file = None
-    density = 300
-    depth = 8
-    imageformat = "jpg"
-    psm = 3
-    quiet_flag = 1
+    '''
+    arguments['<input_path>'] = os.path.realpath(arguments['<input_path>'])
+    arguments['<output_path>'] = os.path.realpath(arguments['<output_path>'])
+    arguments['--density'] = int(arguments['--density'])
+    arguments['--depth'] = int(arguments['--depth'])
+    arguments['--psm'] = int(arguments['--psm'])
+    assert os.path.exists(arguments['<input_path>'])
+    assert arguments['<input_path>'].endswith(".pdf")
 
-    # command-line arguments
-    try:
-        # gnu_getopt allows interspersing of option and non-option arguments
-        opts, args = getopt.gnu_getopt(argv, "hi:o:d:b:f:p:q:", ["help", "input=", "output=", "density=", "depth=", "imageformat=","psm=","quiet="])
-    except getopt.GetoptError:
-        usage()
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            usage()
-            sys.exit()
-        elif opt in ("-i", "--input"):
-            input_file = arg
-        elif opt in ("-o", "--output"):
-            output_file = arg
-        elif opt in ("-d", "--density"):
-            density = int(arg)
-        elif opt in ("-b", "--depth"):
-            depth = int(arg)
-        elif opt in ("-f", "--imageformat"):
-            imageformat = arg
-        elif opt in ("-p", "--psm"):
-            psm = int(arg)
-        elif opt in ("-q", "--quiet"):
-            quiet_flag = int(arg)
-
-    # check if the file provided as argument exists
-    if not os.path.exists(input_file):
-        sys.exit('ERROR: Input file \'%s\' was not found!' % input_file)
-
-    # check if input file is a PDF
-    if not input_file.endswith(".pdf"):
-        sys.exit('ERROR: Input file should be a PDF.')
-
-    # extract dirname, base, ext
-    dirname = os.path.dirname(input_file)
-    base, ext = os.path.splitext(os.path.basename(input_file))
-
-    # specify output file
-    if None == output_file:
-        if "" == dirname:
-            output_file = base + "_ocr.txt"
-        else:
-            output_file = dirname + "/" + base + "_ocr.txt"
-
-    # get number of pages
-    num_pages = PdfFileReader(file(input_file)).getNumPages()
-    print("Number of pages: %d" % num_pages)
+    input_file = PdfFileReader(open(arguments['<input_path>']))
+    num_pages = input_file.getNumPages()
 
     # make random directory
     created_dir_flag = False
